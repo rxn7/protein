3a67082a187680e769e5bcc42388e76919b037fd
Rotthin <kermit2322@gmail.com>
2021-10-11 16:51:57 +0200

    pyramids example finished

--- examples/pyramids/main.c
+++ examples/pyramids/main.c
@@ -8,10 +8,16 @@
 int main(int argc, const char* argv[]){
 	if(!pn_init("Protein", 800, 640)) return -1;
 
+	// Set the clear color to light gray.
 	pn_set_clear_color((pn_color_t){ 80, 80, 80, 255});
+
+	// Hide and lock the mouse.
 	pn_lock_mouse();
+
+	// Enable the camera movement using mouse.
 	pn_enable_mouse_camera_movement();
 
+	// Create a new primitive object of type pyramid.
 	pn_render_object_t* obj = pn_create_primitive(PN_PYRAMID);
 
 	float counter=0.0f;
@@ -19,9 +25,13 @@ int main(int argc, const char* argv[]){
 	f32* forward;
 	f32* right;
 
+	// Set the camera's position Z value to -20.
 	__pn_cam_instance->m_pos[2] = -20;
 
+	// Game loop
 	while(pn_should_run()){
+		// You need to call this at the start of every frame.
+		// This function clears the buffers, calculates the time delta between frames and poll the events. 
 		pn_start_frame();
 
 		forward = (v3){ __pn_cam_instance->m_forward[0], 0, __pn_cam_instance->m_forward[2] };
@@ -44,6 +54,7 @@ int main(int argc, const char* argv[]){
 		pn_v3_mult_each(move_dir, __pn_delta_time * MOVE_SPEED);
 		pn_move_camera(move_dir);
 
+		// Set the object's rotation on Y axis to counter.
  		obj->m_transform.m_rot[1] = counter;
 
 		// Set object's scale.
@@ -60,6 +71,7 @@ int main(int argc, const char* argv[]){
 					// Set object's color.
 					obj->m_color = (pn_color_t) {(f32)(x + HALF) / MAX * 255, (f32)(y + HALF) / MAX * 255, (f32)(z + HALF) / MAX * 255, 255};
 
+					// Set the obj's position.
 					pn_v3_set(obj->m_transform.m_pos, (v3){x,y,z});
 					pn_v3_mult_each(obj->m_transform.m_pos, 4);
 
@@ -69,12 +81,17 @@ int main(int argc, const char* argv[]){
 			}
 		}
 
+		// You need to call this at the end of every frame.
+		// This function swaps the screen buffer.
 		pn_end_frame();
 
 		counter += __pn_delta_time;
 	}
 
+	// You need to free each object created with pn_create_render_object or pn_create_primitive_render_object.
 	pn_free_render_object(obj);
+
+	// This function closes the window and cleans up the memory allocated by protein.
 	pn_exit();
 
 	return 0;
